a7c07d43757f3fd981dbe8129a5bdfbea3bc7c56
Dinolan-Naidoo <1842861@students.wits.ac.za>
2022-07-01 08:59:34 +0200

    Update blender.js

--- Javascript/blender.js
+++ Javascript/blender.js
@@ -32,7 +32,7 @@ scene.background = new THREE.Color(0xffffff);
 
 scene.add(light);
 
-camera.position.set(0,2,7);
+camera.position.set(0,1,6);
 
 function animate(){
     requestAnimationFrame(animate);
